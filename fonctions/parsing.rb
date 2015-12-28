93fdf9fe685c514f26009858e05f09615cbcd5ac
Eugène Nélou <eugene.nelou@larez.fr>
2015-12-28 17:39:54 +0100

    ajout gem Mongoid + course_head comme sample de logs

--- fonctions/parsing.rb
+++ fonctions/parsing.rb
@@ -1,47 +1,53 @@
 require 'rubygems'
 require 'json'
+require 'mongoid'
+
+Mongoid.load!("config/mongoid.yml", :development)
+
+class Page
+  
+  attr_accessor :id, :type, :display_name, :graded, :format, :children
+  
+  include Mongoid::Document
+  
+  def initialize(params, blocks)
+    @id = params['id']
+    @type = params['type']
+    @display_name = params['display_name']
+    @graded = params['graded']
+    @format = params['format']
+    @children = []
+    c = params['children']
+    for child in c
+      page = Page.new(blocks[child], blocks)
+      @children << page
+    end
+    self.mongoize.save
+  end
+end
 
 file = File.new('data/course_structure_ENSCachan_20003S02_Trimestre_1_2015.json', 'r')
 cours = JSON.parse(file.gets)
 #on a désormais un hash du JSON
 
 root = cours['root']
-
 blocks = cours['blocks']
-i = 0
-for page, content in blocks
-  puts(page)
-  i = i+1
-end
-puts(i)
 
-class Page
-  attr_accessor :id, :type, :display_name, :graded, :format, :children
-  def initialize(params, blocks)
-    self.id = params['id']
-    self.type = params['type']
-    self.display_name = params['display_name']
-    self.graded = params['graded']
-    self.format = params['format']
-    self.children = []
-    c = params['children']
-    for child in c
-      page = Page.new(blocks[child], blocks)
-      @children << page
-    end
-  end
-end
-
+#page racine
 classe = Page.new(blocks[root], blocks)
 
+#à remplacer par le remplissage de la BDD
 file2 = File.new('data/course', 'w')
-def tree(page, offset, file)
-  puts(offset+page.display_name)
-  file.write(offset+page.display_name+"\n")
-  offset = offset + " "
+
+#parcours l'arborescence en prenant en argument la page racine et la profondeur
+def tree(page, depth, file)
+  file.write(depth+page.display_name+"\n")
+  depth = depth + " "
   for page in page.children
-    tree(page, offset, file)
+    tree(page, depth, file)
   end
 end
 
 tree(classe, "", file2)
+
+puts(Page.count)
