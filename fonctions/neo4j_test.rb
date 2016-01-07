23bdbf8fd3e611917c859f747e5813b4f586df90
Eugène Nélou <eugene.nelou@larez.fr>
2016-01-07 20:03:45 +0100

    parsage du cours via neo4j opérationnel (pas propre pour autant

--- fonctions/neo4j_test.rb
+++ fonctions/neo4j_test.rb
@@ -1,40 +1,25 @@
 require 'neo4j'
-
-db = Neo4j::Session.open(:server_db)
+require 'json'
 
 class Page
   include Neo4j::ActiveNode
   
-  property :id, constraint: :unique
   property :type, default: 'unknown'
   property :display_name
   property :graded, type: Boolean, default: false
   property :forma, type: String, default: ""
+  property :id, constraint: :unique
   
   validates :display_name, presence: true
   
   has_one :in, :parent, type: :page, model_class: :Page
   has_many :out, :children, type: :page, model_class: :Page
-  
-  
-  
-=begin
-  def initialize(params, blocks)
-    @id = params['id']
-    @type = params['type']
-    @display_name = params['display_name']
-    @graded = params['graded']
-    @format = params['format']
-    @children = []
-    c = params['children']
-    for child in c
-      page = Page.create(blocks[child], blocks)
-      @children << page
-    end
-    self.save
-  end
-=end
 end
+
+db = Neo4j::Session.open(:server_db)
+db.query.match('n').optional_match('n-p-').delete('n, p')
+puts(Page.count + " Pages")
+puts("DB cleared !")
 
 file = File.new('data/course_structure_ENSCachan_20003S02_Trimestre_1_2015.json', 'r')
 cours = JSON.parse(file.gets)
@@ -52,19 +37,19 @@ file2 = File.new('data/course', 'w')
 #parcours l'arborescence en prenant en argument la page racine et la profondeur
 def tree(blocks, id, depth, file)
   page = Page.new(blocks[id])
-  puts(page.display_name)
   file.write(depth+page.display_name+"\n")
   depth = depth + " "
   children_id = blocks[id]["children"]
-  children = []
+  page.children = []
   for child_id in children_id
-    child = tree(blocks, child, depth, file)
+    child = tree(blocks, child_id, depth, file)
     child.save
-    children << child
+    page.children << child
   end
   return page
 end
 
-tree(blocks, root, "", file2)
+cours = tree(blocks, root, "", file2)
+cours.save
 
-puts(Page.count)
+puts(Page.count + "Pages")
