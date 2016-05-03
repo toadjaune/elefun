ffc2adcd77f645ca20afdfc5ddd0b5d263c50598
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2016-05-03 21:29:51 +0200

    Ajout et configuration de quelques utilitaires pratiques

--- iface_user/test/models/script_test.rb
+++ iface_user/test/models/script_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: scripts
+#
+#  id         :integer          not null, primary key
+#  nom        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ScriptTest < ActiveSupport::TestCase
