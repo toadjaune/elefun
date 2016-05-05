b0a6600453cc8023f15827196e5ca47ff90bdb98
Green <green@larez.fr>
2016-05-05 20:45:59 +0200

    Début de frontend qui marche pas car ya rien encore

--- iface_user/app/models/fichier.rb
+++ iface_user/app/models/fichier.rb
@@ -1,3 +1,14 @@
 class Fichier < ActiveRecord::Base
   belongs_to :mooc
+
+  def new(params)
+    origin = params[:tempfile]
+    destination = '/fichiers'
+
+    if File.exists? File.join(destination, File.basename(origin))
+      FileUtils.move origin, File.join(destination, "1-#{File.basename(origin)}")
+    else
+      FileUtils.move origin, File.join(destination, File.basename(origin))
+    end
+  end
 end
