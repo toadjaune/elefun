a31009b575d9dc90375e5f4177e4e65d4adfbca6
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2016-05-08 16:08:56 +0200

    Lancement des scripts en background fonctionnel. Il va falloir restructurer le code qui le fait.

--- iface_user/app/models/bdd_script_association.rb
+++ iface_user/app/models/bdd_script_association.rb
@@ -16,9 +16,38 @@
 #  index_bdd_script_associations_on_script_id  (script_id)
 #
 
+# Permet le lancement du script
+require 'rbconfig'
+require 'open3'
+include Open3
+
 class BddScriptAssociation < ActiveRecord::Base
+
+  # Permettre le lancement asynchrone
+  include Sidekiq::Worker
+
+  def self.etats_valides
+    [ 'Jamais lancé',
+      'En cours',
+      'En attente',
+      'Réussi',
+      'Échoué' ]
+  end
+
   belongs_to :bdd
   belongs_to :script
 
   validates_presence_of :bdd, :script
+  validates :etat,
+    presence: true,
+    inclusion: { in: self.etats_valides }
+
+  before_validation do |a|
+    a.etat ||= BddScriptAssociation.etats_valides[0]
+  end
+
+
+  def perform(script_name, opts)
+    Script.where(nom: script_name).first.launch(opts)
+  end
 end
